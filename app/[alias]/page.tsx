b6d68ffa431e34c54315bb18b1433b03ce6fab08
Your Name <albertzz@bu.edu>
2025-04-17 19:26:54 -0400

    fixed alias\page.tsx

--- app/[alias]/page.tsx
+++ app/[alias]/page.tsx
@@ -1,15 +1,11 @@
-export const dynamic = "force-dynamic";
-
 import { redirect } from "next/navigation";
 import { connectToDB } from "@/lib/db";
 
-interface Props {
-    params: { alias: string };
-}
+export default async function AliasPage({ params }: { params: Promise<{ alias: string }> }) {
+    const { alias } = await params;
 
-export default async function AliasPage({ params }: Props) {
     const db = await connectToDB();
-    const record = await db.collection("urls").findOne({ alias: params.alias });
+    const record = await db.collection("urls").findOne({ alias });
 
     if (!record) {
         return <div className="p-4 text-center">Alias not found</div>;
